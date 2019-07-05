4d5b3a43e379a1b441c282e96d665e9574ec31cd
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-07-05 01:28:21 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute.js';
 import { history } from './helpers';
 
